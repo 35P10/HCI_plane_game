a9b7fc71f25e40c25b3efab77c23d39a7afda405
E5P10 <lucca360@outlook.com>
2022-10-22 11:29:00 -0500

    feat: game > fuel UI

--- Assets/scripts/player__status.cs
+++ Assets/scripts/player__status.cs
@@ -2,16 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class player__status : MonoBehaviour
-{
+public class player__status : MonoBehaviour{
+    public GameObject obj__row_fuel;
     public static Vector3 last_checkpoint = Vector3.zero;
-    public static float fuel  = 10000;
+    public static float default_fuel  = 50000;
+    public static float fuel;
+
+    private void Start() {
+        fuel = default_fuel;
+    }
 
     private void Update() {
         if(player__status.fuel > 0){
             fuel -= player__movement.currentSpeed;
-            Debug.Log("Fuel" + fuel);
-            Debug.Log("currentSpeed" + player__movement.currentSpeed);
+            obj__row_fuel.transform.localRotation = Quaternion.Euler(0, 140 * fuel / default_fuel, 0);
         }
     }
 }
